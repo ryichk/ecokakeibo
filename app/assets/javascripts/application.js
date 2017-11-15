6c64ea562220d7e23efc1f3ad9cf2a44a9a1d73b
Ichiki Ryo <tohodaiichiki@gmail.com>
2017-11-15 16:26:07 +0900

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,8 @@
 // about supported directives.
 //
 
-//= require jquery
+
 //= require jquery.turbolinks
-//= require jquery_ujs
 //= require bootstrap
 //= require chartkick
 //= require rails-ujs
