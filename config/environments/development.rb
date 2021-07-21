19eb36746ecc9fe9ba0f23b3e8fcb420c17739b5
ryichk <ryichk@gmail.com>
2021-07-21 19:21:31 +0900

    :tada: introduced docker to make it work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'localhost:3000'
+  host = 'localhost:8080'
   config.action_mailer.default_url_options = { host: host }
 
   config.action_mailer.perform_caching = false
@@ -60,5 +60,5 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
